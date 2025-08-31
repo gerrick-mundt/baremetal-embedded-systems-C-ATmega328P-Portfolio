7c36ec6628483b96e587c76bad77421ca4156241
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-08-31 15:19:54 -0400

    Add files via upload

---
+++ Project 1_ATmega328P_Blink-Built-in LED/blinkLED_main.c
@@ -0,0 +1,27 @@
+// Defines names of ports and pins, individual bits and
+// other registers
+#include <avr/io.h>
+// Provides simple software delay functions
+#include <util/delay.h>
+
+int main(void)
+{
+    // Set PORTB5 as an output
+    DDRB |= (1 << PB5);
+
+    while(1)
+    {
+        // Set Port B Pin 5 (PB5) high 
+        // PORTB = PORTB | (1 << PB5);
+        PORTB |= (1 << PB5);
+
+        // Wait
+        _delay_ms(1000);
+
+        // Unset Port B Pin 5 (PB5)
+        PORTB &= ~(1 << PB5);
+
+        // Wait some more
+        _delay_ms(1000);
+    }
+}
