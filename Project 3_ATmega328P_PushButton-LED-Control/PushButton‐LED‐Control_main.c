804aef63e6d173d1fc35eaf1aa487a77968aa2db
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-09-02 17:59:48 -0400

    Add files via upload

---
+++ Project 3_ATmega328P_PushButton-LED-Control/PushButton‐LED‐Control_main.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+    // Write PD2 as an input
+    DDRD &= ~(1 << PD2);
+
+    // Make PD2 pull-up resistor configuration (PD2 baseline HIGH)
+    PORTD |= (1 << PD2);
+
+    // Write PD7 as an output
+    DDRD |= (1 << PD7);
+
+    // Generate while loop that continuously checks for input signal
+    // and writes PD7 high whenever the button switch attached to 
+    // PD2 is high
+
+    while(1) 
+    {
+        // Read state of PD2
+        if(PIND & (1 << PD2))
+        {
+            // Set PD7 as high (lights the LED)
+            PORTD |= (1 << PD7);
+        }
+
+        else
+        {
+            // Set PD7 as low (Leaves LED in off state)
+            PORTD &= ~(1 << PD7);
+        }
+    }
+}
