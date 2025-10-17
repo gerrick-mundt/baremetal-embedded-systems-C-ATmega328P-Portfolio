6987517d135ee31ad50e87ec7b5d156389104e75
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-10-17 13:35:44 -0400

    Update ATmega328P_portWrite.h

--- Custom Utilities/ATmega328P_portWrite.h
+++ Custom Utilities/ATmega328P_portWrite.h
@@ -8,6 +8,6 @@
 bool uint8t_portCheck(volatile uint8_t *reg);
 
 // Port write function
-void uint8t_PortWrite(volatile uint8_t *port, uint8_t pin, bool value);
+void pw8(volatile uint8_t *port, uint8_t pin, bool value);
 
 #endif
