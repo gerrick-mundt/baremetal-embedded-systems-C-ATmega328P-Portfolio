867165ae8f52847c658574e8d05aebd2daa00598
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-10-17 19:42:32 -0400

    Update ATmega328P_common.h

--- Custom Utilities/ATmega328P_portWrite.h
+++ Custom Utilities/ATmega328P_portWrite.h
@@ -1,13 +1,33 @@
-#ifndef ATMEGA328P_PORTWRITE_H
-#define ATMEGA328P_PORTWRITE_H
-
+#ifndef ATMEGA328P_COMMON_H
+#define ATMEGA328P_COMMON_H
 #include <stdint.h>
-#include <stdbool.h>
 
-// Port check function
-bool uint8t_portCheck(volatile uint8_t *reg);
+//--------------------------------------------------
+// GPIO Type Definitions
+//--------------------------------------------------
 
-// Port write function
-void pw8(volatile uint8_t *port, uint8_t pin, bool value);
+// Pin state for GPIO
+typedef enum
+{
+    LOW = 0,
+    HIGH = 1
+} pinState_t;
+
+// Pin direction for GPIO
+typedef enum
+{
+    INPUT = 0,
+    OUTPUT = 1
+} pinDirection_t;
+
+//--------------------------------------------------
+// GPIO Functions
+//--------------------------------------------------
+
+// Pin Write Function - Writes pin state HIGH or LOW
+void gpio_write(volatile uint8_t *port, uint8_t pin, pinState_t state);
+
+// Pin Direction Set Function - Sets a pin as an input or an output
+void gpio_direction(volatile uint8_t *ddr, uint8_t pin, pinDirection_t direction);
 
 #endif
